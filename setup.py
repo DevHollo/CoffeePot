ca4d623e6d925064ef7fbc93caae2a8fbf3f9b96
Hollo <127456155+DevHollo@users.noreply.github.com>
2025-10-25 15:08:15 -0600

    Refactor setup.py for package name and versioning

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
-from .vars import VERSION
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'coffeepot', 'vars.py')) as f:
+    VERSION = re.search(r'VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', f.read()).group(1)
 
 setup(
-    name='coffeepot',
+    name='coffeepot-http',
     version=VERSION,
     description='A cozy Python server and module for brewing coffee virtually',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
+    author='Hollo',
     author_email='hollo1234567890e@gmail.com',
-    url='https://github.com/DevHollo/coffeepot',
+    url='https://github.com/DevHollo/CoffeePot',
     packages=find_packages(),
     python_requires='>=3.10',
     install_requires=[],
